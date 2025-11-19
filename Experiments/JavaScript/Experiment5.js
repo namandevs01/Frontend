c84a074e85cc92f79c06f8b69874346e5ccb2381
Naman Sharma <namansh.devs@gmail.com>
2025-11-19 18:51:14 +0530

    small changes

--- Experiments/JavaScript/Experiment5.js
+++ Experiments/JavaScript/Experiment5.js
@@ -1,10 +1,10 @@
 
-// Task 12: Welcome Message App
+
 let userName = prompt("What is your name?");
 alert("Welcome, " + userName + "!");
 document.write("<h2>Hello " + userName + "! Glad to have you here.</h2>");
 
-// Using ternary operator
+
 let message = (age >= 18) ? "You are eligible to vote!" : "You are not eligible yet.";
 alert(message);
 document.write("<p>" + message + "</p>");
